eb7a9d5b95740a9b1bcbb63ff0c9368c5f6f1c1a
adamohern <adam@mechanicalcolor.com>
2016-12-18 13:21:31 -0500

    removed group remove mode

--- lxserv/tagger_setMaterial_groups.py
+++ lxserv/tagger_setMaterial_groups.py
@@ -16,13 +16,6 @@ class CommandClass(tagger.Commander):
                     'value': tagger.DEFAULT_GROUP_NAME,
                     'flags': []
                 }, {
-                    'name': tagger.OPERATION,
-                    'label': tagger.LABEL_OPERATION,
-                    'datatype': 'string',
-                    'value': tagger.ADD,
-                    'popup': tagger.POPUPS_ADD_REMOVE,
-                    'flags': ['optional']
-                }, {
                     'name': tagger.PRESET,
                     'label': tagger.LABEL_PRESET,
                     'datatype': 'string',
@@ -34,24 +27,17 @@ class CommandClass(tagger.Commander):
 
     def commander_execute(self, msg, flags):
         group_name = self.commander_arg_value(0)
-        operation = self.commander_arg_value(1, tagger.ADD)
-        preset = self.commander_arg_value(2, tagger.RANDOM)
+        preset = self.commander_arg_value(1, tagger.RANDOM)
 
         preset = None if preset == tagger.RANDOM else preset
 
         selmode = tagger.selection.get_mode()
 
-        if operation == tagger.ADD:
-            group = tagger.items.group_selected_and_maskable(group_name)
-            mask = tagger.shadertree.build_material( group, preset = preset )
-            tagger.shadertree.move_to_base_shader(mask)
-
-        if operation == tagger.REMOVE:
-            items = tagger.items.get_selected_and_maskable()
-            groups = tagger.items.get_groups(selected)
-            tagger.shadertree.seek_and_destroy(groups)
+        group = tagger.items.group_selected_and_maskable(group_name)
+        mask = tagger.shadertree.build_material( group, preset = preset )
+        tagger.shadertree.move_to_base_shader(mask)
 
         notifier = tagger.Notifier()
         notifier.Notify(lx.symbol.fCMDNOTIFY_DATATYPE)
-        
+
 lx.bless(CommandClass, NAME_CMD)
